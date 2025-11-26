78c03472b31ca754952e9796c4a22711b1802710
atharvky7 <atharvky7@gmail.com>
2025-11-26 21:38:58 +0530

    Update relu.v

--- cnn_accel_structure/src/relu.v
+++ cnn_accel_structure/src/relu.v
@@ -1,1 +1,16 @@
-// TODO: relu.v implementation
+`timescale 1ns/1ps
+`include "cnn_params.vh"
+
+// relu.v
+// ReLU activation: y = max(0, x)
+
+module relu #(
+    parameter DATA_WIDTH = `DATA_WIDTH
+)(
+    input  wire signed [DATA_WIDTH-1:0] x,
+    output wire signed [DATA_WIDTH-1:0] y
+);
+
+    assign y = (x < 0) ? {DATA_WIDTH{1'b0}} : x;
+
+endmodule
